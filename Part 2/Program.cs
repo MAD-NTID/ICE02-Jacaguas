b60c3f19ea453bec01ee9ada6bbe61403262591f
xamarin_2355 <xamarin_2355@ad.rit.edu>
2019-08-28 11:13:23 -0400

    My first commit on GitHub!

---
+++ Part 2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Part_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("I'm from Rochester, New York");
+        }
+    }
+}
---
+++ Part 3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Part_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Hello MAD students-I'm Hector Rivera");
+            Console.WriteLine("");
+            Console.WriteLine("I'm from Rochester, New York");
+            Console.WriteLine("");
+
+        }
+    }
+}
